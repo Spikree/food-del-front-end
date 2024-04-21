698b9aa028b207affe63c6260bce4b58189e9839
Spikree <avimahalingpure10@gmail.com>
2024-04-21 13:12:14 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import { assets } from '../../assets/assets'
 import './Footer.css'
 
 export default function Footer() {
+
+    const date = new Date();
+
     return <>
         <div className="footer" id='footer'>
             <div className="footer-content">
@@ -14,13 +17,27 @@ export default function Footer() {
                         <img src={assets.linkedin_icon} alt="" />
                     </div>
                 </div>
-                <div className="footer-content-right">
-
-                </div>
                 <div className="footer-content-center">
-
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>  
+                    <ul>
+                        <li>1-22-33-44</li>
+                        <li>contact@company.com</li>
+                    </ul>
                 </div>
             </div>
+            <hr />
+            <p className="footer-copyright">
+                copyright {date.getFullYear()} Tomato.com - All Rights Reserveed.
+            </p>
         </div>
     </>
 }
