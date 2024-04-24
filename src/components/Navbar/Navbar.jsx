146e65213b3e8c8024661eab935336bf6c909cf0
Spikree <avimahalingpure10@gmail.com>
2024-04-24 17:41:22 +0530

    ..

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar({setShowLogin}) {
                 <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</a>
             </ul>
             <div className="navbar-right">
-                <img src={assets.search_icon} alt="" />
+                {/* <img src={assets.search_icon} alt="" /> */}
                 <div className="navbar-search-icon">
                     <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                     <div className={getTotalCartAmount()===0?"":"dot"}>
