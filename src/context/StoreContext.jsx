2ba80f657516c0a6184e82b988b1cc9f8a058528
Spikree <avimahalingpure10@gmail.com>
2024-05-20 22:51:52 +0530

    change

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,13 +1,13 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
-
 export const StoreContext = createContext(null);
+import axios from "axios";
 
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
     const url = "http://localhost:4000"
     const [token,setToken] = useState("");
+    const [food_list,setFoodList] = useState([])
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -33,10 +33,19 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    const fetchFoodList = async () => {
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data)
+    }
+
     useEffect(()=> {
-        if(localStorage.getItem("token")) {
-            setToken(localStorage.getItem("token"));
+        async function loadData() {
+            await fetchFoodList();
+            if(localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+            }
         }
+        loadData();
     },[])
 
     const contextValue = {
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Cart() {
 
-    const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+    const {cartItems,food_list,removeFromCart,getTotalCartAmount,url} = useContext(StoreContext);
     const navigate = useNavigate();
 
     return <>
@@ -26,7 +26,7 @@ export default function Cart() {
                         return(
                             <div key={index}>
                                  <div className='cart-items-title cart-items-item'>
-                                <img src={item.image} alt="" />
+                                <img src={url+"/images/"+item.image} alt="" />
                                 <p>{item.name}</p>
                                 <p>${item.price}</p>
                                 <p>{cartItems[item._id]}</p>
