ab5967cf4d9b9a857f50edeedbd494cd0a4f57b2
Avi <avimahalingpure10@gmail.com>
2024-06-24 09:30:19 +0530

    live

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://food-del-app-backend-cgyq.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
